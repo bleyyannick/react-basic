526f2aebeee1a44e093975e7f87b188b76e56d4c
Yannick Bley <bleyyannickpro@gmail.com>
2022-07-22 11:50:45 +0200

    handling state and UI reactivity

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,24 +1,24 @@
-import { useState } from "react"
+import Card from './shared/Card'; 
+import PropTypes from 'prop-types'; 
+import { FaTimes } from 'react-icons/fa'; 
 
-
-function FeedbackItem() {
-
- const [rating, setRating ] = useState(7); 
- const [text, setText] = useState('This is an example of a feedback Item.')
- const handleClick = () =>{
-    setRating((prevState) =>{
-        return prevState + 1; 
-    })
- }
+function FeedbackItem({item, handleDelete}) {
   return (
-    <div className="card">
-        <div className="num-display">{rating}</div>
+    <Card >
+        <div className="num-display">{item.rating}</div>
+        <button onClick={() => handleDelete(item.id)} className='close'>
+          <FaTimes color='purple'/>
+        </button>
         <div className="text-display">
-            {text}
+            {item.text}
         </div>
-        <button onClick={handleClick}>Click</button>
-    </div>
+    </Card>
   )
 }
 
-export default FeedbackItem
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+}
+
+export default FeedbackItem; 
+
